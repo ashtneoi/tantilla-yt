70160c24102a455819369534b84bc0bee0007a55
ashtneoi <ashtneoi@gmail.com>
2018-04-13 01:39:58 -0700

    Use status instead of abort
    abort doesn't work the way I had guessed.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bakery import render_path
 from config import config
-from tantilla import create_app, HTMLResponse
+from tantilla import create_app, HTMLResponse, status
 
 
 MOUNT_POINT = config["mount_point"]
@@ -8,7 +8,7 @@ MOUNT_POINT = config["mount_point"]
 
 def hello(req):
     if req.method == 'POST':
-        return abort(400)
+        return status(req, 400)
     return HTMLResponse(
         render_path("tmpl/hello.htmo", {
             "base": MOUNT_POINT,
